3cbed339169348f354b212983feaa24c53634f32
PikachuHy <2931408816@qq.com>
2017-07-19 16:53:02 +0800

    去掉头部测试，使用dtk日志组件

--- main.cpp
+++ main.cpp
@@ -5,21 +5,25 @@
 #include <QtCore/QCoreApplication>
 #include <common.h>
 #include "controller.h"
-
-using std::cout;
+#include <DApplication>
+#include <dlabel.h>
+#include <DPlatformWindowHandle>
+#include <DLog>
+#include <dutility.h>
+#include <DUtil>
 using QSS::Controller;
 using QSS::Profile;
 Profile profile;
-
+DWIDGET_USE_NAMESPACE
 bool readConfig(const QString &file) {
     QFile c(file);
     if (!c.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "can't open config file " << file << endl;
+        qDebug() << "can't open config file " << file;
         return false;
     }
     if (!c.isReadable()) {
         qDebug() << "config file " << file
-                 << " is not readable!" << endl;
+                 << " is not readable!";
         return false;
     }
     QByteArray confArray = c.readAll();
@@ -39,35 +43,9 @@ bool readConfig(const QString &file) {
 
     return true;
 }
-
-bool headerTest() {
-    int length;
-    bool unused_auth;
-    QHostAddress test_addr("1.2.3.4");
-    QHostAddress test_addr_v6("2001:0db8:85a3:0000:0000:8a2e:1010:2020");
-    quint16 test_port = 56;
-    QSS::Address test_res, test_v6(test_addr_v6, test_port);
-    QByteArray packed = QSS::Common::packAddress(test_v6);
-    QSS::Common::parseHeader(packed, test_res, length, unused_auth);
-    bool success = (test_v6 == test_res);
-    if (!success) {
-        QSS::Common::qOut << test_v6.toString() << " --> "
-                          << test_res.toString() << endl;
-    }
-    packed = QSS::Common::packAddress(test_addr, test_port);
-    QSS::Common::parseHeader(packed, test_res, length, unused_auth);
-    bool success2 = ((test_res.getFirstIP() == test_addr)
-                     && (test_res.getPort() == test_port));
-    if (!success2) {
-        QSS::Common::qOut << test_addr.toString().toLocal8Bit()
-                          << ":" << test_port << " --> "
-                          << test_res.toString() << endl;
-    }
-    return success & success2;
-}
-
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+    Dtk::Util::DLogManager::registerConsoleAppender();
     bool success = readConfig("/etc/ss/ss.json");
     if (!success) {
         qDebug() << "讀取文件失敗";
@@ -83,12 +61,6 @@ int main(int argc, char *argv[]) {
         qDebug() << "[QSS::Controller::info]" << log;
     });
     controller->setup(profile);
-    if (!headerTest()) {
-        QSS::Common::qOut << "Header test failed";
-        return false;
-    } else {
-        qDebug() << "頭部測試成功";
-    }
     success = controller->start();
     if (!success) {
         qDebug() << "啓動失敗";
