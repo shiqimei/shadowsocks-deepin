2c13b107213ff2374fc6be16707c95f0490af535
PikachuHy <2931408816@qq.com>
2017-07-23 15:49:55 +0800

    编辑在线pac的界面

---
+++ EditOnlinePacUrlDialog.h
@@ -0,0 +1,22 @@
+#ifndef EDITONLINEPACURLDIALOG_H
+#define EDITONLINEPACURLDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class EditOnlinePacUrlDialog;
+}
+
+class EditOnlinePacUrlDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit EditOnlinePacUrlDialog(QString url, QWidget *parent= nullptr);
+    ~EditOnlinePacUrlDialog();
+    QString getOnlinePacUrl();
+private:
+    Ui::EditOnlinePacUrlDialog *ui;
+};
+
+#endif // EDITONLINEPACURLDIALOG_H
