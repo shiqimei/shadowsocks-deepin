b864ae1cff1d1d78498d448f4e8e7563290e280c
PikachuHy <2931408816@qq.com>
2017-07-25 21:48:44 +0800

    添加ss-qt5的辅助类

---
+++ SsValidator.cpp
@@ -0,0 +1,66 @@
+#include "SsValidator.h"
+#include <QtShadowsocks>
+
+QStringList SSValidator::supportedMethodList()
+{
+    QList<QByteArray> methodBA = QSS::Cipher::getSupportedMethodList();
+    QStringList methodList;
+    for (const QByteArray &method : methodBA) {
+        methodList.push_back(QString(method).toUpper());
+    }
+    return methodList;
+}
+
+bool SSValidator::validate(QString input)
+{
+    //must begin with ss:// to distinguish from random base64 encoded strings
+    if (input.startsWith("ss://")) {
+        input.remove(0, 5);
+        QStringList tagList = input.split('#');
+        QString decode(QByteArray::fromBase64(tagList.first().toUtf8()));
+        QStringList decList = decode.split(':');
+        if (decList.size() < 3) {
+            return false;
+        }
+
+        //Validate Method
+        QString method = decList.first().toUpper();
+        if (method.endsWith("-AUTH")) {
+            method.remove("-AUTH");
+        }
+        if (!validateMethod(method)) {
+            return false;
+        }
+
+        //Validate Port
+        QString port = decList.last();
+        if (!validatePort(port)) {
+            return false;
+        }
+
+        //Validate whether server and password exist
+        QStringList pwdServer = decList.at(1).split('@');
+        if (pwdServer.size() < 2) {
+            return false;
+        }
+
+        //it seems acceptable now
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool SSValidator::validatePort(const QString &port)
+{
+    bool ok;
+    port.toUShort(&ok);
+    return ok;
+}
+
+bool SSValidator::validateMethod(const QString &method)
+{
+    static const QStringList validMethodList = supportedMethodList();
+    return validMethodList.contains(method, Qt::CaseInsensitive);
+}
---
+++ SsValidator.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2014-2016 Symeon Huang <hzwhuang@gmail.com>
+ *
+ * shadowsocks-qt5 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * shadowsocks-qt5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libQtShadowsocks; see the file LICENSE. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SSVALIDATOR_H
+#define SSVALIDATOR_H
+#include <QString>
+#include <QStringList>
+
+class SSValidator
+{
+public:
+    static bool validate(QString input);
+    static bool validatePort(const QString &port);
+    static bool validateMethod(const QString &method);
+
+    /*
+     * Return supported encryption method list at run-time
+     * To avoid repetitive query, please store return result as static.
+     */
+    static QStringList supportedMethodList();
+};
+
+#endif // SSVALIDATOR_H
