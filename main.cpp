e4425088cc285f9eeafa99aadeea327f4f971799
PikachuHy <2931408816@qq.com>
2017-08-05 19:20:06 +0800

    程序修改为单例运行

--- main.cpp
+++ main.cpp
@@ -26,17 +26,26 @@ DWIDGET_USE_NAMESPACE
 int main(int argc, char *argv[]) {
 
     DApplication::loadDXcbPlugin();
-    DApplication a(argc, argv);
-    a.setOrganizationName("pikachu");
-    a.setApplicationName("shadowsocks-client");
-    a.setApplicationVersion("0.0.1");
-    qApp->setQuitOnLastWindowClosed(false);
-    a.setTheme("light");
-    QApplication::setWindowIcon(QIcon(":/icons/shadowsocks.ico"));
-    Dtk::Util::DLogManager::registerConsoleAppender();
-    Dtk::Util::DLogManager::registerFileAppender();
-    SystemTrayIcon systemTrayIcon(&a);
-    systemTrayIcon.show();
+    DApplication app(argc, argv);
+    if(app.setSingleInstance("shadowsocks-client")){
+        app.setOrganizationName("pikachu");
+        app.setApplicationName("shadowsocks-client");
+        app.setApplicationVersion("0.0.1");
 
-    return QApplication::exec();
+        app.setProductIcon(QPixmap::fromImage(QImage(":/icons/Resources/shadowsocks-client.png")));
+        app.setProductName("Shadowsocks Client");
+        app.setApplicationDescription("If you want to keep a secret, you must also hide it from yourself.");
+        app.setApplicationAcknowledgementPage("https://github.com/PikachuHy/shadowsocks-client");
+
+        app.setTheme("light");
+        QApplication::setWindowIcon(QIcon(":/icons/shadowsocks.ico"));
+        app.setQuitOnLastWindowClosed(false);
+        Dtk::Util::DLogManager::registerConsoleAppender();
+        Dtk::Util::DLogManager::registerFileAppender();
+        SystemTrayIcon systemTrayIcon(&app);
+        systemTrayIcon.show();
+        return QApplication::exec();
+
+    }
+    return 0;
 }
