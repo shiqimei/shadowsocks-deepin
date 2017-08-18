7e1a243ca8509b28e9c99f2b0a2c7579ca71e9bb
PikachuHy <2931408816@qq.com>
2017-08-18 15:04:23 +0800

    重构目录结构

---
+++ src/service/impl/UpdateServiceImpl_old_impl.cpp
@@ -0,0 +1,20 @@
+//
+// Created by pikachu on 17-8-18.
+//
+
+#include "UpdateServiceImpl_old_impl.h"
+
+void UpdateServiceImpl_old_impl::checkUpdate() {
+    updateChecker.checkUpdate();
+}
+
+void UpdateServiceImpl_old_impl::setCheckForUpdatesAtStartup(bool b) {
+}
+
+void UpdateServiceImpl_old_impl::setCheckPrereleaseVersion(bool b) {
+
+}
+
+void UpdateServiceImpl_old_impl::updateLocalPacFromGFWList() {
+
+}
---
+++ src/service/impl/UpdateServiceImpl_old_impl.h
@@ -0,0 +1,31 @@
+//
+// Created by pikachu on 17-8-18.
+//
+
+#ifndef SHADOWSOCKS_CLIENT_UPDATESERVICEIMPL_OLD_IMPL_H
+#define SHADOWSOCKS_CLIENT_UPDATESERVICEIMPL_OLD_IMPL_H
+
+#include <service/UpdateService.h>
+#include <UpdateChecker.h>
+#include "common/QCore.h"
+#include "common/QGui.h"
+#include "common/QWidgets.h"
+#include "common/dtk.h"
+
+class UpdateServiceImpl_old_impl : public UpdateService, public QObject {
+Q_OBJECT
+public:
+    void checkUpdate() override;
+
+    void setCheckForUpdatesAtStartup(bool b) override;
+
+    void setCheckPrereleaseVersion(bool b) override;
+
+    void updateLocalPacFromGFWList() override;
+
+private:
+    UpdateChecker updateChecker;
+};
+
+
+#endif //SHADOWSOCKS_CLIENT_UPDATESERVICEIMPL_OLD_IMPL_H
