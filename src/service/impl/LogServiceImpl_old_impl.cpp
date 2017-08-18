18aed917927c17b5150d4dfe13350a8f14ebc6dd
PikachuHy <2931408816@qq.com>
2017-08-18 14:56:15 +0800

    初步完成日志服务

---
+++ src/service/impl/LogServiceImpl_old_impl.cpp
@@ -0,0 +1,13 @@
+//
+// Created by pikachu on 17-8-18.
+//
+
+#include "LogServiceImpl_old_impl.h"
+
+void LogServiceImpl_old_impl::showLog() {
+    showLogWidget.show();
+}
+
+void LogServiceImpl_old_impl::setVerboseLogging(bool b) {
+
+}
---
+++ src/service/impl/LogServiceImpl_old_impl.h
@@ -0,0 +1,29 @@
+//
+// Created by pikachu on 17-8-18.
+//
+
+#ifndef SHADOWSOCKS_CLIENT_LOGSERVICEIMPL_OLD_IMPL_H
+#define SHADOWSOCKS_CLIENT_LOGSERVICEIMPL_OLD_IMPL_H
+
+#include <service/LogService.h>
+#include "common/QCore.h"
+#include "common/QGui.h"
+#include "common/QWidgets.h"
+#include "common/dtk.h"
+
+#include <ShowLogWidget.h>
+
+class LogServiceImpl_old_impl : public LogService, public QObject {
+Q_OBJECT
+public:
+
+    void showLog() override;
+
+    void setVerboseLogging(bool b) override;
+
+private:
+    ShowLogWidget showLogWidget;
+};
+
+
+#endif //SHADOWSOCKS_CLIENT_LOGSERVICEIMPL_OLD_IMPL_H
