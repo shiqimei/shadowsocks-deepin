293683fa1d312ec7d020da83f1b6b0049602a39b
PikachuHy <2931408816@qq.com>
2017-12-09 16:26:59 +0800

    修复第一次启动时因为日志文件退出

--- src/widget/ShowLogWidget.cpp
+++ src/widget/ShowLogWidget.cpp
@@ -10,8 +10,13 @@ ShowLogWidget::ShowLogWidget(QWidget *parent) : QWidget(parent) {
     setMinimumWidth(1200);
     plainTextEdit = new QPlainTextEdit();
     QFile logFile(Util::LOG_PATH);
+    if (!logFile.exists()){
+        logFile.open(QIODevice::WriteOnly);
+        logFile.close();
+    }
     if (!logFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
         qDebug() << "打开日志文件失败";
+        QMessageBox::critical(nullptr,"ERROR","Open log file fail");
         exit(0);
     }
     plainTextEdit->setPlainText(logFile.readAll());
