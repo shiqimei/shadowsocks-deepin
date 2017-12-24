41fa073f44b4d51c9f6acb9430cfb78c3df6fda6
PikachuHy <2931408816@qq.com>
2017-12-24 21:19:17 +0800

    添加根据信号退出代码

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,30 @@
 #include "MainWindow.h"
 #include <DApplication>
 #include <DLog>
+#include <csignal>
+
 DWIDGET_USE_NAMESPACE
 #ifdef DUTIL_USE_NAMESPACE
 DUTIL_USE_NAMESPACE
 #else
 DCORE_USE_NAMESPACE
 #endif
+static void onSignalRecv(int sig)
+{
+    if (sig == SIGINT || sig == SIGTERM) {
+        qApp->quit();
+    } else {
+        qWarning("Unhandled signal %d", sig);
+    }
+}
+
 int main(int argc, char *argv[]) {
     DApplication::loadDXcbPlugin();
     DApplication app(argc, argv);
     if (app.setSingleInstance(Util::APPLICATION_NAME)) {
         qRegisterMetaTypeStreamOperators<SQProfile>("SQProfile");
+        signal(SIGINT, onSignalRecv);
+        signal(SIGTERM, onSignalRecv);
         app.setOrganizationName(Util::ORGANIZATION_NAME);
         app.setApplicationName(Util::APPLICATION_NAME);
         app.setApplicationVersion(Util::VERSION);
@@ -32,5 +45,6 @@ int main(int argc, char *argv[]) {
 //        mainWindow.show();
         return QApplication::exec();
     }
+    QMessageBox::critical(nullptr,QObject::tr("critical"),QObject::tr("application has started!!!"));
     return 0;
 }
