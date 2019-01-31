db77b2b3e355e5f4c0cc2cd67003b270e17e5e94
lolimay <lolimay@lolimay.cn>
2019-02-01 00:20:14 +0800

    update CMakeLists.txt

--- src/main.cpp
+++ src/main.cpp
@@ -8,50 +8,51 @@
 DWIDGET_USE_NAMESPACE
 DCORE_USE_NAMESPACE
 
-static void onSignalRecv(int sig) {
-    if (sig == SIGINT || sig == SIGTERM) {
-        qApp->quit();
-    } else {
-        qWarning("Unhandled signal %d", sig);
-    }
-}
+// static void onSignalRecv(int sig) {
+//     if (sig == SIGINT || sig == SIGTERM) {
+//         qApp->quit();
+//     } else {
+//         qWarning("Unhandled signal %d", sig);
+//     }
+// }
 
 int main(int argc, char *argv[]) {
-    DApplication::loadDXcbPlugin();
+    // DApplication::loadDXcbPlugin();
     DApplication app(argc, argv);
-    app.setAttribute(Qt::AA_UseHighDpiPixmaps);
-    const QString socket_path(QString("shadowsocks-deepin_%1").arg(getuid()));
-    if (app.setSingleInstance(socket_path)) {
-        signal(SIGINT, onSignalRecv);
-        signal(SIGTERM, onSignalRecv);
-        app.setTheme("light");
-        app.loadTranslator();
+    // app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+    // const QString socket_path(QString("shadowsocks-deepin_%1").arg(getuid()));
 
-        const QString descriptionText = QApplication::tr(
-                    "If you want to keep a secret, you must also hide it from yourself.");
+    // if (app.setSingleInstance(socket_path)) {
+    //     signal(SIGINT, onSignalRecv);
+    //     signal(SIGTERM, onSignalRecv);
+    //     app.setTheme("light");
+    //     app.loadTranslator();
 
-        const QString acknowledgementLink = "https://github.com/lolimay/shadowsocks-deepin";
+    //     const QString descriptionText = QApplication::tr(
+    //                 "If you want to keep a secret, you must also hide it from yourself.");
 
-        app.setOrganizationName("pikachu");
-        app.setApplicationName("shadowsocks-deepin");
-        app.setApplicationDisplayName(QObject::tr("Shadowsocks Deepin"));
-        app.setApplicationVersion("1.1.0");
+    //     const QString acknowledgementLink = "https://github.com/lolimay/shadowsocks-deepin";
 
-        app.setProductIcon(QIcon(Utils::getIconQrcPath("ssw128.svg")));
-        app.setProductName(QApplication::tr("Shadowsocks Deepin"));
-        app.setApplicationDescription(descriptionText);
-        app.setApplicationAcknowledgementPage(acknowledgementLink);
+    //     app.setOrganizationName("pikachu");
+    //     app.setApplicationName("shadowsocks-deepin");
+    //     app.setApplicationDisplayName(QObject::tr("Shadowsocks"));
+    //     app.setApplicationVersion("1.3.0");
 
-        app.setWindowIcon(QIcon(Utils::getIconQrcPath("ssw128.png")));
-        app.setQuitOnLastWindowClosed(false);
-        DLogManager::registerConsoleAppender();
-        DLogManager::registerFileAppender();
-        MainWindow window;
+    //     app.setProductIcon(QIcon(Utils::getIconQrcPath("ssw128.svg")));
+    //     app.setProductName(QApplication::tr("Shadowsocks Deepin"));
+    //     app.setApplicationDescription(descriptionText);
+    //     app.setApplicationAcknowledgementPage(acknowledgementLink);
 
-        QObject::connect(&app, &DApplication::newInstanceStarted, &window, &MainWindow::activateWindow);
+    //     app.setWindowIcon(QIcon(Utils::getIconQrcPath("ssw128.png")));
+    //     app.setQuitOnLastWindowClosed(false);
+    //     DLogManager::registerConsoleAppender();
+    //     DLogManager::registerFileAppender();
+    //     MainWindow window;
 
-        Dtk::Widget::moveToCenter(&window);
-        return app.exec();
-    }
+    //     QObject::connect(&app, &DApplication::newInstanceStarted, &window, &MainWindow::activateWindow);
+
+    //     Dtk::Widget::moveToCenter(&window);
+    //     return app.exec();
+    // }
     return 0;
 }
